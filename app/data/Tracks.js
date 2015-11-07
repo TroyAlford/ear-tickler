beb1b4de88e7ffcadc9a3ed6f81e2cd3f6d90e28
Troy Alford <troy@troyalford.com>
2015-11-06 20:40:52 -0800

    ta - added more sounds and finished clean-up

---
+++ app/data/Tracks.js
@@ -0,0 +1,25 @@
+module.exports = [{
+  name: 'Birds (Light)',
+  origin: 'https://www.freesound.org/data/previews/1/1050_1112-hq.mp3'
+},{
+  name: 'Birds (Sparse)',
+  origin: 'https://www.freesound.org/data/previews/158/158527_2783336-hq.mp3'
+},{
+  name: 'Crickets',
+  origin: 'https://www.freesound.org/data/previews/39/39829_28216-hq.mp3'
+},{
+  name: 'Roar - Dragon (Fast)',
+  origin: 'https://www.freesound.org/data/previews/85/85568_1206321-hq.mp3'
+},{
+  name: 'Fast River',
+  origin: 'https://www.freesound.org/data/previews/39/39831_28216-hq.mp3'
+},{
+  name: 'Magic (Fast)',
+  origin: 'https://www.freesound.org/data/previews/221/221683_1015240-hq.mp3'
+},{
+  name: 'Thunder Storm',
+  origin: 'https://www.freesound.org/data/previews/2/2523_1112-hq.mp3'
+},{
+  name: 'Roar - Monster (Slow)',
+  origin: 'https://www.freesound.org/data/previews/267/267454_3415022-hq.mp3'
+}];
