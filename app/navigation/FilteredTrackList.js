e6cdd7b1b1d6ef655d2efcfa0e1f237ab5c18456
Troy Alford <troy@troyalford.com>
2015-11-06 20:15:31 -0800

    ta - clean-up of folder structure

---
+++ app/navigation/FilteredTrackList.js
@@ -0,0 +1,32 @@
+var React = require('react');
+var TrackSearchBar = require('./../TrackSearchBar.js');
+var TrackList = require('./../TrackList.js');
+
+var FilteredTrackList = React.createClass({
+    getInitialState: function() {
+        return {
+            filterText: ''
+        };
+    },
+    handleFilterChange: function(filterText) {
+        this.setState({
+            filterText: filterText
+        });
+    },
+    render: function() {
+        return (
+            <div className="filtered-track-list">
+                <TrackSearchBar
+                    filterText={this.state.filterText}
+                    onFilterChange={this.handleFilterChange}
+                />
+                <TrackList
+                    filterText={this.state.filterText}
+                    tracks={this.props.tracks}
+                />
+            </div>
+        );
+    }
+});
+
+module.exports = FilteredTrackList;
