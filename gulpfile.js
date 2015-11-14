6f6b663fa603ea5f9817ff8aca6fbe2b36d72917
Troy Alford <troy@troyalford.com>
2015-11-14 10:06:20 -0800

    ta - rebuilt gulpfile.js and removed test framework

--- gulpfile.js
+++ gulpfile.js
@@ -15,187 +15,115 @@ var glob = require('glob');
 var express = require('gulp-express');
 var less = require('gulp-less');
 var livereload = require('gulp-livereload');
-var jasminePhantomJs = require('gulp-jasmine2-phantomjs');
 
-// External dependencies you do not want to rebundle while developing,
-// but include in your application deployment
-var dependencies = [
-	'react',
-  'react/addons',
-  'flux-react'
+var paths = {
+  develop: './builds/develop',
+  release: './builds/release'
+}
+
+var bundlerForApplicationJS = browserify({
+  entries: './app/main.js',   // Only need initial file, browserify finds the rest
+  transform: [babelify],      // We want to convert JSX to normal javascript
+  debug: true,                // Gives us sourcemapping
+
+  cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
+}).external([
+  // Do not rebundle these into main.
+  'react', 'react/addons', 'flux-react'
+]);
+var bundlerForVendorJS = browserify({
+  debug: true,
+  require: ['react', 'react/addons', 'flux-react']
+});
+
+function rebuildAppJs() {
+  var startTime = Date.now();
+  console.log(startTime + ': Repackaging Application JavaScript...');
+  bundlerForApplicationJS.bundle()
+    .on('error', gutil.log)
+    .pipe(source('main.js'))
+    .pipe(gulp.dest(paths.develop + '/js'))
+    .pipe(livereload())
+    .pipe(notify(function () {
+      var finishTime = Date.now();
+      console.log('DEVELOP: main.js produced in ' + (finishTime - startTime) + 'ms');
+    }))
+    .pipe(streamify(uglify()))
+    .pipe(gulp.dest(paths.release + '/js'))
+    .pipe(notify(function() {
+      var finishTime = Date.now();
+      console.log('RELEASE: main.js produced in ' + (finishTime - startTime) + 'ms');
+    }));
+}
+function rebuildVendorJs() {
+  var startTime = Date.now();
+  console.log(startTime + ': Repackaging Vendor JavaScript...');
+  bundlerForVendorJS.bundle()
+    .on('error', gutil.log)
+    .pipe(source('vendors.js'))
+    .pipe(gulp.dest(paths.develop + '/js'))
+    .pipe(livereload())
+    .pipe(notify(function () {
+      var finishTime = Date.now();
+      console.log('DEVELOP: vendors.js produced in ' + (finishTime - startTime) + 'ms');
+    }))
+    .pipe(streamify(uglify()))
+    .pipe(gulp.dest(paths.release + '/js'))
+    .pipe(notify(function() {
+      var finishTime = Date.now();
+      console.log('RELEASE: vendors.js produced in ' + (finishTime - startTime) + 'ms');
+    }));
+}
+function rebuildCss() {
+  var startTime = Date.now();
+  console.log(startTime + ': Repackaging Application CSS...');
+  gulp.src('./styles/**/*.{css,less}')
+    .pipe(less())
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest(paths.develop + '/css'))
+    .pipe(notify(function() {
+      var finishTime = Date.now();
+      console.log('DEVELOP: main.css produced in ' + (finishTime - startTime) + 'ms');
+    }))
+    .pipe(cssmin())
+    .pipe(gulp.dest(paths.release + '/css'))
+    .pipe(notify(function() {
+        var finishTime = Date.now();
+        console.log('RELEASE: main.css produced in ' + (finishTime - startTime) + 'ms');
+    }));
+}
+
+var assetMap = [
+  { name: 'HTML Pages',   src: './app/**/*.html',             dest: '' },
+  { name: 'Font Assets',  src: './fontello/font/**/*',        dest: '/font' },
+  { name: 'Image Assets', src: './images/**/*.{gif,jpg,png}', dest: '/images' }
 ];
-
-var browserifyTask = function (options) {
-
-  // Our app bundler
-	var appBundler = browserify({
-		entries: [options.src], // Only need initial file, browserify finds the rest
-   	transform: [babelify], // We want to convert JSX to normal javascript
-		debug: options.development, // Gives us sourcemapping
-		cache: {}, packageCache: {}, fullPaths: options.development // Requirement of watchify
-	});
-
-	// We set our dependencies as externals on our app bundler when developing.
-  // You might consider doing this for production also and load two javascript
-  // files (main.js and vendors.js), as vendors.js will probably not change and
-  // takes full advantage of caching
-	appBundler.external(options.development ? dependencies : []);
-
-
-  // The rebundle process
-  var rebundle = function () {
-    var start = Date.now();
-    console.log('Building APP bundle');
-    appBundler.bundle()
-      .on('error', gutil.log)
-      .pipe(source('main.js'))
-      .pipe(gulpif(!options.development, streamify(uglify())))
-      .pipe(gulp.dest(options.dest))
-      .pipe(gulpif(options.development, livereload()))
-      .pipe(notify(function () {
-        console.log('APP bundle built in ' + (Date.now() - start) + 'ms');
-      }));
-  };
-
-  // Fire up Watchify when developing
-  if (options.development) {
-    appBundler = watchify(appBundler);
-    appBundler.on('update', rebundle);
-  }
-      
-  rebundle();
-
-  // We create a separate bundle for our dependencies as they
-  // should not rebundle on file changes. This only happens when
-  // we develop. When deploying the dependencies will be included 
-  // in the application bundle
-  if (options.development) {
-
-  	var testFiles = glob.sync('./specs/**/*-spec.js');
-		var testBundler = browserify({
-			entries: testFiles,
-			debug: true, // Gives us sourcemapping
-			transform: [babelify],
-			cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
-		});
-
-    testBundler.external(dependencies);
-
-  	var rebundleTests = function () {
-  		var start = Date.now();
-  		console.log('Building TEST bundle');
-  		testBundler.bundle()
-      .on('error', gutil.log)
-	      .pipe(source('specs.js'))
-	      .pipe(gulp.dest(options.dest))
-	      .pipe(livereload())
-	      .pipe(notify(function () {
-	        console.log('TEST bundle built in ' + (Date.now() - start) + 'ms');
-	      }));
-  	};
-
-    testBundler = watchify(testBundler);
-    testBundler.on('update', rebundleTests);
-    rebundleTests();
-
-    // Remove react-addons when deploying, as it is only for testing
-    if (!options.development) {
-      dependencies.splice(dependencies.indexOf('react/addons'), 1);
-    }
-
-    var vendorsBundler = browserify({
-      debug: true,
-      require: dependencies
-    });
-    
-    // Run the vendor bundle
-    var start = new Date();
-    console.log('Building VENDORS bundle');
-    vendorsBundler.bundle()
-      .on('error', gutil.log)
-      .pipe(source('vendors.js'))
-      .pipe(gulpif(!options.development, streamify(uglify())))
-      .pipe(gulp.dest(options.dest))
-      .pipe(notify(function () {
-        console.log('VENDORS bundle built in ' + (Date.now() - start) + 'ms');
-      }));
-    
-  }
-  
-}
-
-var cssTask = function (options) {
-    if (options.development) {
-      var run = function () {
-        console.log(arguments);
-        var start = new Date();
-        console.log('Building CSS bundle');
-        gulp.src(options.src)
-          .pipe(less())
-          .pipe(concat('main.css'))
-          .pipe(gulp.dest(options.dest))
-          .pipe(notify(function () {
-            console.log('CSS bundle built in ' + (Date.now() - start) + 'ms');
-          }));
-      };
-      run();
-      gulp.watch(options.src, run);
-    } else {
-      gulp.src(options.src)
-        .pipe(concat('main.less'))
-        .pipe(cssmin())
-        .pipe(gulp.dest(options.dest));   
-    }
-}
-
-// Starts our development workflow
-gulp.task('default', function () {
-
-  browserifyTask({
-    development: true,
-    src:  './app/main.js',
-    dest: './build'
+function rebuildAssets() {
+  assetMap.forEach(function(mapping) {
+    var startTime = Date.now();
+    console.log(startTime + ': Repackaging ' + mapping.name + '...')
+    gulp.src(mapping.src)
+      .pipe(gulp.dest('./builds/develop' + (mapping.dest || '')))
+      .pipe(gulp.dest('./builds/release'  + (mapping.dest || '')))
+      .pipe(notify({ onLast: true, message: function() {
+        var finishTime = Date.now();
+        console.log(mapping.name + ' repackaged in ' + (finishTime - startTime) + 'ms');
+      }}));
   });
-  
-  cssTask({
-    development: true,
-    src:  './styles/**/*.{css,less}',
-    dest: './build/css'
-  });
-  gulp.src('./fontello/font/**/*')
-    .pipe(gulp.dest('./build/font'));
+};
 
-  gulp.src('./app/**/*.html')
-    .pipe(gulp.dest('./build'));
-
-  gulp.src('./images/**/*.{gif,jpg,png}')
-    .pipe(gulp.dest('./build/images'));
+gulp.task('build', function() {
+  rebuildAppJs();
+  rebuildVendorJs();
+  rebuildCss();
+  rebuildAssets();
 });
-
-gulp.task('deploy', function () {
-
-  browserifyTask({
-    development: false,
-    src:  './app/main.js',
-    dest: './deploy'
+gulp.task('watch', function() {
+  watchify(bundlerForApplicationJS).on('update', rebuildAppJs);
+  watchify(bundlerForVendorJS).on('update', rebuildVendorJs);
+  gulp.watch('./styles/**/*.{css,less}', rebuildCss);
+  assetMap.forEach(function(mapping) {
+    gulp.watch(mapping.src, rebuildAssets);
   });
-  
-  cssTask({
-    development: false,
-    src:  './styles/**/*.{less,css}',
-    dest: './deploy/css'
-  });
-  gulp.src('./fontello/font/**/*')
-    .pipe(gulp.dest('./deploy/font'));
-
-  gulp.src('./app/**/*.html')
-    .pipe(gulp.dest('./deploy'));
-
-  gulp.src('./images/**/*.{gif,jpg,png}')
-    .pipe(gulp.dest('./deploy/images'));
-
 });
-
-gulp.task('test', function () {
-    return gulp.src('./build/testrunner-phantomjs.html').pipe(jasminePhantomJs());
-});
+gulp.task('default', ['build', 'watch']);
