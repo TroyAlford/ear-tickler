5a46facf95e5cb789f7b96f7ae72039401532e99
Troy Alford <troy.alford@mycase.com>
2015-11-26 11:32:54 -0800

    ta - updated click-to-seek for React 0.14

--- app/components/playback/AudioProgressBar.js
+++ app/components/playback/AudioProgressBar.js
@@ -4,8 +4,8 @@ var TimeFormatterMixin = require('../../mixins/TimeFormatterMixin.js');
 module.exports = React.createClass({
   mixins: [TimeFormatterMixin],
   handleClick: function(event) {
-    var bar_el  = this,
-        coords  = bar_el.getClientRects()[0],
+    var bar     = this.refs.progress_bar,
+        coords  = bar.getClientRects()[0],
         click_x = event.pageX - coords.left,
         percent = click_x / (coords.right - coords.left);
 
@@ -15,9 +15,9 @@ module.exports = React.createClass({
     var current = this.secondsToTime(this.props.position);
     var duration = this.secondsToTime(this.props.duration);
     return (
-      <div
-        className="audio-progress-bar"
-        onClick={this.handleClick}>
+      <div className="audio-progress-bar"
+           ref="progress_bar"
+           onClick={this.handleClick}>
         <div className="audio-timer">{current} / {duration}</div>
         <div
           className="audio-progress-bar-fill"
