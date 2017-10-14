cc642fbe7ff938ae7c5159ba37a1308af89790be
Troy Alford <troy@troyalford.com>
2017-10-14 02:29:18 -0700

    Refactoring components

--- source/application/Application.js
+++ source/application/Application.js
@@ -1,26 +1,12 @@
-var React    = require('react');
-var ReactDOM = require('react-dom');
-var Fluxxor  = require('fluxxor');
-var _        = require('lodash');
+import React    from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 
-var Layout          = require('./Layout.js');
-var TrackStore      = require('../stores/TrackStore.js');
-var PlayerStore     = require('../stores/PlayerStore.js');
-var VisualizerStore = require('../stores/VisualizerStore.js');
+import store from '../stores'
+import Layout from './Layout'
 
-var stores = {
-  PlayerStore: new PlayerStore(),
-  TrackStore: new TrackStore(),
-  VisualizerStore: new VisualizerStore()
-};
-var actions = {};
-Object.keys(stores).forEach(function(key) {
-  _.extend(actions, stores[key].actions);
-});
-
-window.flux = new Fluxxor.Flux(stores, actions);
-flux.on('dispatch', function(type, payload) {
-  console && console.log && console.log('[Dispatch]', type, payload);
-});
-
-ReactDOM.render(<Layout flux={flux} />, document.getElementById('application'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Layout />
+  </Provider>
+, document.getElementById('application'))
--- source/application/Layout.js
+++ source/application/Layout.js
@@ -1,80 +1,28 @@
-var React             = require('react');
-var Fluxxor           = require('fluxxor');
-var _ = window._      = require('lodash');
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-var FilteredTrackList = require('../components/navigation/FilteredTrackList.js');
-var Oscilloscope      = require('../components/visualization/Oscilloscope.js');
-var SoundBoard        = require('../components/soundboard/SoundBoard.js');
-var Visualizer        = require('../components/visualization/Visualizer.js');
+import FilteredTrackList from '../components/navigation/FilteredTrackList'
+import SoundBoard from '../components/soundboard/SoundBoard'
+import Visualizer from '../components/visualization/Visualizer'
 
-var Guid              = require('../helpers/Guid.js');
-var XHR               = require('../helpers/XHR.js');
-
-module.exports = React.createClass({
-  mixins: [
-    Fluxxor.FluxMixin(React),
-    Fluxxor.StoreWatchMixin('TrackStore', 'PlayerStore', 'VisualizerStore')
-  ],
-
-  getStateFromFlux: function() {
-    var flux = this.getFlux();
-    return {
-      tracks:      flux.store('TrackStore').tracks,
-      players:     flux.store('PlayerStore').players,
-      visualizers: flux.store('VisualizerStore').visualizers
-    };
-  },
-
-  handleAddAudioPlayer: function(track_id) {
-    this.getFlux().actions.addPlayer(this.state.tracks[track_id]);
-  },
-  handleCloseAudioPlayer: function(player_id) {
-    this.getFlux().actions.removePlayer(player_id);
-  },
-  handleRemoveTrack: function(track_id) {
-    this.getFlux().actions.removeTrack(track_id);
-  },
-  handleUpdateTrack: function(track) {
-    this.getFlux().actions.updateTrack(track);
-  },
-
-  _as_array: function(state_object) {
-    return Object.keys(this.state[state_object]).map(function(key) {
-      return this.state[state_object][key]
-    }.bind(this));
-  },
-
-  render: function() {
-    var addedTrackIds = Object.keys(this.state.tracks);
-
+export default class Layout extends Component {
+  render() {
     return(
       <div className="ear-tickler application">
         <div className="header-bar">
           <div className="title">
             <i className="tickle-logo"></i> Ear Tickler
           </div>
-          <Visualizer
-            players={this._as_array('players')}
-            visualizers={this._as_array('visualizers')}
-          />
+          <Visualizer />
           <div className="social-media">
             <a href="https://github.com/troyalford/ear-tickler" target="_blank">
               <i className="tickle-github"></i>
             </a>
           </div>
         </div>
-        <FilteredTrackList
-          tracks={this._as_array('tracks')}
-          addedTrackIds={addedTrackIds}
-          onPlayTrack={this.handleAddAudioPlayer}
-          onRemoveTrack={this.handleRemoveTrack}
-          onUpdateTrack={this.handleUpdateTrack}
-        />
-        <SoundBoard
-          players={this._as_array('players')}
-          onCloseClicked={this.handleCloseAudioPlayer}
-        />
+        <FilteredTrackList />
+        <SoundBoard />
       </div>
-    );
+    )
   }
-});
+}
