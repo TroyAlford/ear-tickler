ef4e6900ad36ff70a0ab60d503a194cf414011d2
Troy Alford <troy@troyalford.com>
2017-10-15 00:12:01 -0700

    Added service-worker for offline audio caching

--- source/application/Application.js
+++ source/application/Application.js
@@ -5,6 +5,9 @@ import { Provider } from 'react-redux'
 import store from '../stores'
 import Layout from './Layout'
 
+import registerServiceWorkers from '../service-workers/register'
+registerServiceWorkers()
+
 ReactDOM.render(
   <Provider store={store}>
     <Layout />
