7dc06189f49058a21af12be366e2352a015dfa6c
Troy Alford <troy@troyalford.com>
2017-10-15 03:33:06 -0700

    Merging Oscilloscope into Visualizer

--- source/components/visualization/Visualizer.js
+++ source/components/visualization/Visualizer.js
@@ -1,115 +1,110 @@
 import React, { Component } from 'react'
-import guid from '../../helpers/Guid.js'
+import { connect } from 'react-redux'
 
 import Oscilloscope from './Oscilloscope'
 
-export default class Visualizer extends Component {
+class Visualizer extends Component {
   constructor(props) {
     super(props)
 
-    this.attachInterval = null // Interval for attaching to new AudioNode objects
-    this.analyzer = null       // A Web Audio API AnalyserNode object, for analyzing audio output
-    this.merger = null         // A Web Audio API ChannelMergerNode, for combining Audio outputs to analyze
-    this.svg = null            // The <svg> element to render to
-    this.uniqueId = `visualizer.${guid()}`
+    // Force Howler to setup & create an audio context
+    if (!Howler.ctx) Howler.volume(1)
+    const { ctx } = Howler
 
-    this.state = {
-      timeData: [],
-      frequencyData: []
-    }
-  }
+    ctx.analyzer = ctx.createAnalyser()
+    ctx.analyzer.fftSize = 2048 // Default setting
 
-  componentDidMount() {
-    // this.loadVisualizer()
-    // this.onResize()
-    // window.addEventListener('resize', this.onResize)
+    ctx.merger = ctx.createChannelMerger(2)
+    ctx.merger.connect(ctx.analyzer)
 
-    // this.start()
-  }
-  componentWillUnmount() {
-    // this.stop()
-    // this.unloadVisualizer()
-  }
-  onResize() {
-    this.setState({
-      height: this.refs.svg.offsetHeight,
-      width:  this.refs.svg.offsetWidth
-    })
-  }
+    this.attachAudioNodes = this.attachAudioNodes.bind(this)
+    this.renderPath = this.renderPath.bind(this)
 
-  onAudioLoaded(player) {
-    player.howl._sounds.forEach(function(sound) {
-      if (player.howl._webAudio) {
-        sound._node.connect(this.merger)
-      } else if (player.howl._html5) {
-        sound.crossOrigin = "anonymous"
-        sound.webAudioNode = this.audioContext.createMediaElementSource(sound._node)
-        sound.webAudioNode.connect(this.merger)
-      }
-      player._merger = this.merger
-    }.bind(this))
-  }
-
-  start() {
-    this.stop()
-    this.updateInterval = setInterval(this.redraw, 33) // 30 fps
-  }
-  stop() {
-    clearInterval(this.updateInterval || 0)
-  }
-
-  redraw() {
-    var bufferLength = this.analyzer.frequencyBinCount
-    var dataArray = new Uint8Array(bufferLength)
-
-    this.analyzer.getByteTimeDomainData(dataArray)
-    this.setState({ timeData: dataArray })
-  }
-
-  loadVisualizer() {
-    this.audioContext = Howler.ctx
-    this.analyzer = this.audioContext.createAnalyser()
-    this.analyzer.fftSize = 2048 // Default setting
-
-    this.merger = this.audioContext.createChannelMerger(2)
-    this.merger.connect(this.analyzer)
-
-    this.svg = this.refs.svg
-  }
-  unloadVisualizer() {
-    clearInterval(this.refresh || 0)
+    this.attachAudioNodes()
   }
 
   componentWillReceiveProps(nextProps) {
-    this.props.players.filter(function(player) {
-      return player.howl !== undefined && player._merger === undefined
-    }.bind(this)).forEach(function(player) {
-      this.onAudioLoaded(player)
-    }.bind(this))
+    this.reattach = true
+  }
+  componentDidUpdate() {
+    if (this.reattach) {
+      this.attachAudioNodes()
+      this.reattach = false
+    }
+  }
+
+  attachAudioNodes() {
+    const { _howls: howls, ctx } = Howler
+
+    howls
+    .filter(howl => !howl.merged)
+    .forEach((howl) => {
+      howl._sounds.forEach((sound) => {
+        if (howl._webAudio) sound._node.connect(ctx.merger)
+        if (howl._html5) {
+          sound.crossOrigin = 'anonymous'
+          sound.webAudioNode = ctx.createMediaElementSource(sound._node)
+          sound.webAudioNode.connect(ctx.merger)
+        }
+      })
+
+      howl.merged = true
+    })
+  }
+
+  componentDidMount() {
+    this.refreshInterval = setInterval(this.forceUpdate.bind(this), 33)
+  }
+  componentWillUnmount() {
+    clearInterval(this.refreshInterval)
+  }
+
+  renderPath() {
+    if (!this.svg) return null
+
+    const { analyzer } = Howler.ctx
+    const bufferLength = analyzer.frequencyBinCount
+    let timeData = new Uint8Array(bufferLength)
+
+    analyzer.getByteTimeDomainData(timeData)
+
+    const { height, width } = this.svg.getBoundingClientRect()
+    const sliceWidth = width / timeData.length
+    const hCenter    = height / 2
+
+    const coordinates = Array.from(timeData).map((value, index) => {
+      const offsetY = (value - 128)
+
+      const x = Math.round(sliceWidth * index)
+      const y = Math.round(hCenter + (offsetY * 1.5))
+
+      return `${x},${y}`
+    })
+
+    const path = `M${coordinates.join('L')}`
+    return <path d={path} stroke="url(#rainbow)" strokeWidth="2" />
   }
 
   render() {
-    return null
-
-    const gradientId = "rainbow"
     return (
-      <svg className="visualizer" ref="svg">
-        <linearGradient id={gradientId} x1="0%" y1="0%" x2="100%" y2="0%">
-          <stop offset="0%"   style={{ stopOpacity: 1, stopColor: "red" }}    />
-          <stop offset="16%"  style={{ stopOpacity: 1, stopColor: "orange" }} />
-          <stop offset="33%"  style={{ stopOpacity: 1, stopColor: "yellow" }} />
-          <stop offset="50%"  style={{ stopOpacity: 1, stopColor: "green" }}  />
-          <stop offset="66%"  style={{ stopOpacity: 1, stopColor: "blue" }}   />
-          <stop offset="83%"  style={{ stopOpacity: 1, stopColor: "indigo" }} />
-          <stop offset="100%" style={{ stopOpacity: 1, stopColor: "violet" }} />
-        </linearGradient>
-        <Oscilloscope
-          data={this.state.timeData}
-          stroke={`#${gradientId}`}
-          height={this.state.height}
-          width={this.state.width}
-        />
+      <svg className="visualizer" ref={(self) => { this.svg = self }}>
+        <defs>
+          <linearGradient id="rainbow" x1="0%" y1="0%" x2="100%" y2="0%">
+            <stop offset="0%"   stopOpacity="1" stopColor="#ff0000" />
+            <stop offset="16%"  stopOpacity="1" stopColor="#ffa500" />
+            <stop offset="33%"  stopOpacity="1" stopColor="#ffff00" />
+            <stop offset="50%"  stopOpacity="1" stopColor="#008000" />
+            <stop offset="66%"  stopOpacity="1" stopColor="#0000ff" />
+            <stop offset="83%"  stopOpacity="1" stopColor="#4b0082" />
+            <stop offset="100%" stopOpacity="1" stopColor="#8d38c9" />
+          </linearGradient>
+        </defs>
+        {this.renderPath()}
       </svg>
     )
   }
 }
+
+export default connect(
+  (state) => ({ players: state.players })
+)(Visualizer)
